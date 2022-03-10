8777a8aaef808f012b81c6049a7b6c4a5a0ae47d
ltp1405 <LamTungPro1405@gmail.com>
2022-03-10 20:22:09 +0700

    Update struct attribute

--- School/Course.h
+++ School/Course.h
@@ -1,17 +1,23 @@
 #pragma once
+#include "../School/Session.h"
+#include "../utils/Date.h"
+#include <string>
+using namespace std;
 
 struct CourseRegistrationSession {
-	bool activated;
-	Date start;
-	Date end;
+    bool activated;
+    Date start;
+    Date end;
 };
 
 struct Course {
-	string id;
-	string name;
-	string teacherName;
-	int numberOfCredits;
-	int maxStudents;
-	Session *sessions;
-	const int sessionNumbers = 2;
+    string id;
+    string name;
+    string teacherName;
+    int numberOfCredits;
+    int maxStudents;
+    Session *sessions;
+    const int sessionNumbers = 2;
+
+    void startRegistrationSession();
 };
--- School/SchoolYear.h
+++ School/SchoolYear.h
@@ -1,8 +1,15 @@
 #pragma once
+#include "../School/Semester.h"
+#include "../utils/Date.h"
 
 struct SchoolYear {
-	Date from;
-	Date to;
-	Semester *semesters;
-	int semesterNumber;
+    Date from;
+    Date to;
+    Semester *semesters;
+    int semesterNumber;
+
+    SchoolYear();
+    void addBeginDate();
+    void addEndDate();
+    void addSemester(Semester semester);
 };
