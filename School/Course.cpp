6b139ad928e92af9f931a77df3e1bfc94dee51af
ltp1405 <LamTungPro1405@gmail.com>
2022-03-14 01:04:58 +0700

    🔥 Remove registration session

--- School/Course.cpp
+++ School/Course.cpp
@@ -1,9 +1,5 @@
 #include "Course.h"
 
-RegistrationSession::RegistrationSession() : start(Date(0, 0, 0)), end(Date(0, 0, 0)) {
-    activated = false;
-}
-
 Course::Course(string courseId, string courseName, int numberOfCredits, int maxStudents, string teacher)
     : id(courseId)
     , name(courseName)
--- School/Course.h
+++ School/Course.h
@@ -5,14 +5,6 @@
 #include <string>
 using std::string;
 
-struct RegistrationSession {
-    bool activated;
-    Date start;
-    Date end;
-
-    RegistrationSession();
-};
-
 struct Course {
     const string id;
     const string name;
@@ -20,12 +12,10 @@ struct Course {
     const int credits;
     const int maxStudents;
     LinkedList<Session> sessions;
-    RegistrationSession regSession;
 
     Course(string courseId, string courseName, int numberOfCredits,
            int maxStudents, string teacher);
 
     void addTeacherName(string name);
-    void startRegistrationSession();
     void addSession(Session session);
 };
