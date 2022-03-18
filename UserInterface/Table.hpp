8ebece794d6d57c0203f6613af4d8c56e7999cb4
ltp1405 <LamTungPro1405@gmail.com>
2022-03-18 13:56:36 +0700

    ♻️  Use constant instead of literal

--- UserInterface/Table.hpp
+++ UserInterface/Table.hpp
@@ -8,6 +8,18 @@ using std::string;
 using std::tuple;
 
 
+const string upperRight = "┐";
+const string lowerLeft = "└";
+const string horizontalDown = "┴";
+const string horizontalUp = "┬";
+const string verticalLeft = "├";
+const string horizontal = "─";
+const string horizontalVertical = "┼";
+const string lowerRight = "┘";
+const string upperLeft = "┌";
+const string vertical = "│";
+const string verticalRight = "┤";
+
 enum Alignment {
     center,
     left,
@@ -46,109 +58,15 @@ class Table {
     vector<vector<string>> rows;
 
 private:
-    int getWidth() {
-        int width = 0;
-        for (auto col = cols.begin(); col != cols.end(); col++) {
-            width += col->width;
-        }
-        width += cols.size() + 1;
-        return width;
-    }
-
-    void printTop() {
-        cout << "┌";
-        for (auto col = cols.begin(); col != cols.end(); col++) {
-            for (int i = 0; i < col->width; i++)
-                cout << "─";
-            if (col != cols.end() - 1)
-                cout << "┬";
-            else
-                cout << "┐";
-        }
-        cout << endl;
-    }
-
-    void printBot() {
-        cout << "└";
-        for (auto col = cols.begin(); col != cols.end(); col++) {
-            for (int i = 0; i < col->width; i++)
-                cout << "─";
-            if (col != cols.end() - 1)
-                cout << "┴";
-            else
-                cout << "┘";
-        }
-        cout << endl;
-    }
-
-    void printBotHeader() {
-        cout << "├";
-        for (auto col = cols.begin(); col != cols.end(); col++) {
-            for (int i = 0; i < col->width; i++)
-                cout << "─";
-            if (col != cols.end() - 1)
-                cout << "┼";
-            else
-                cout << "┤";
-        }
-        cout << endl;
-    }
-
+    int getWidth();
+    void printTop();
+    void printBot();
+    void printBotHeader();
+    void printLine(const string s, int times);
 public:
-    Table(string title) {
-        this->title = title;
-    }
-
-    void addColumn(string name) {
-        cols.push_back(Column(name));
-    }
-
-    void addColumn(Column col) {
-        cols.push_back(col);
-    }
-
-    void addRow(vector<string> data) {
-        while (data.size() < cols.size()) {
-            data.push_back("");
-        }
-
-        while (data.size() > cols.size()) {
-            data.pop_back();
-        }
-        rows.push_back(data);
-    }
-
-    void display() {
-        printTop();
-
-        cout << "│";
-        for (auto col = cols.begin(); col != cols.end(); col++) {
-            if (col->align == Alignment::left)
-                cout.setf(std::ios::left);
-            else if (col->align == Alignment::right)
-                cout.setf(std::ios::right);
-            cout.width(col->width);
-            cout << col->name;
-            cout << "│";
-        }
-
-        cout << endl;
-        printBotHeader();
-        for (auto row = rows.begin(); row != rows.end(); row++) {
-            cout << "│";
-            for (int i = 0; i < row->size(); i++) {
-                if (cols[i].align == Alignment::left)
-                    cout.setf(std::ios::left);
-                else if (cols[i].align == Alignment::right)
-                    cout.setf(std::ios::right);
-
-                cout.width(cols[i].width);
-                cout << (*row)[i];
-                cout << "│";
-            }
-
-            cout << endl;
-        }
-        printBot();
-    }
+    Table(string title);
+    void addColumn(string name);
+    void addColumn(Column col);
+    void addRow(vector<string> data);
+    void display();
 };
