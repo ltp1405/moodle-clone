72c85be41c9cc53db8e79cb213b5bac850b90043
Cohvtk <94277200+Cohvtk@users.noreply.github.com>
2022-03-18 19:56:46 +0700

    Small update :poop:

--- Personnel/Student.h
+++ Personnel/Student.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "../School/Course.h"
+#include <string>
+using std::string;
 
 enum Gender {
 	MALE,
@@ -12,8 +15,12 @@ struct Student {
 	string id;
 	Gender gender;
 	Date dateOfBirth;
+	int age;
 
 	Course *enrolledCourse;
 	int courseNumber;
 	const int maxCourses = 5;
 };
+
+void addStudent(string firstname, string lastname, string id, Gender gender, Date dateOfBirth);
+void didplayStudent();
