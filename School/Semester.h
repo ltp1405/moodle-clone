6e57c45aaf217ebe9170613d29c43bd604e2e007
nghiemkythu <nghiemdt2013@gmail.com>
2022-03-19 18:35:03 +0700

    I have fixed the constructor in Semester.h and add template library to this file

--- School/Semester.h
+++ School/Semester.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "../School/Course.h"
 #include "../utils/Date.h"
-
+#include "../utils/LinkedList.h"
 struct Semester {
     int order;
     Date start;
     Date end;
     LinkedList<Course> courses;
 
-    Semester(int order, Date start, Date end);
+    Semester(int order, Date start, Date end){
+        this->order = order;
+        this->start = start;
+        this->end = end;
+    }
     void addtime(int order, Date start, Date end);
     void viewListOfCourse();
     void addCourse(Course course);
