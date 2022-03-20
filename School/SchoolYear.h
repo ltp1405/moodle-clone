3aa618b773db914b36543d9e52da13df2ce39c5d
nghiemkythu <nghiemdt2013@gmail.com>
2022-03-20 14:38:06 +0700

    I have fixed constructor in file SchoolYear.h

--- School/SchoolYear.h
+++ School/SchoolYear.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "../School/Semester.h"
 #include "../utils/Date.h"
-
+#include "../utils/LinkedList.h"
 struct SchoolYear {
     Date from;
     Date to;
     LinkedList<Semester> semesters;
     int semesterNumber;
 
-    SchoolYear(Date from, Date to);
+    SchoolYear();
+    SchoolYear(Date from, Date to){
+        this->from = from;
+        this->to = to;
+    }
     void addtime(Date from, Date to);
     void addSemester(Semester semester);
     bool compare(Semester semester1, Semester semester2);
