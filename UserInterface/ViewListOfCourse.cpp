7fd2a43bfc71b7d6f59f045a13c014a4a97a67ff
ltp1405 <LamTungPro1405@gmail.com>
2022-03-28 12:48:01 +0700

    ✨ Add View list Of Course features

---
+++ UserInterface/ViewListOfCourse.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include "../School/Semester.h"
+#include "../School/SchoolYear.h"
+#include "../School/Course.h"
+#include "../utils/LinkedList.h"
+#include "Table.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
+
+void viewListOfCourseFromSemester(Semester &sm) { 
+    Table tb("List of Course");
+    tb.addColumn("ID");
+    tb.addColumn("Name");
+    tb.addColumn("No of Credits");
+    tb.addColumn("No of Student");
+    tb.addColumn("Teacher Name");
+    tb.addColumn("Max Student");
+
+    DNode<Course> *cur = sm.courses.getHead();
+    while (cur) {
+        tb.addRow(vector<string> {
+                cur->data.id,
+                cur->data.name,
+                std::to_string(cur->data.credits),
+                std::to_string(cur->data.students.getSize()),
+                cur->data.teacherName,
+                std::to_string(cur->data.maxStudents),
+                });
+
+        cur = cur->next;
+    }
+
+    tb.display();
+}
