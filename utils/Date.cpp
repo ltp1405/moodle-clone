7fbeb6912184c4dc40510064a9fb92f5facf1385
ltp1405 <LamTungPro1405@gmail.com>
2022-04-10 13:45:30 +0700

    🎨 More correct Date printing format

--- utils/Date.cpp
+++ utils/Date.cpp
@@ -1,4 +1,7 @@
 #include "Date.h"
+#include <ios>
+#include <sstream>
+using std::to_string;
 
 Date::Date(int day, int month, int year) {
     this->day = day;
@@ -6,9 +9,46 @@ Date::Date(int day, int month, int year) {
     this->year = year;
 }
 
-string Date::toString() {
+string Date::toString(string delim) {
     if (!day || !month || !year)
         return "";
-    string s = to_string(day) + "/" + to_string(month) + "/" + to_string(year);
-    return s;
+    std::stringstream ss;
+    ss.setf(std::ios::showpoint | std::ios::right);
+    ss.width(2);
+    ss.fill('0');
+    ss << day << delim;
+    ss.width(2);
+    ss << month << delim;
+    ss.width(4);
+    ss << year;
+    return ss.str();
+}
+
+bool Date::valid() {
+    if (year < 0)
+        return false;
+    if (month < 1 || month > 12)
+        return false;
+
+    bool isLeap;
+    int numberOfDay;
+    if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0)
+        isLeap = false;
+    else 
+        isLeap = true;
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 ||
+        month == 8 || month == 10 || month == 12)
+        numberOfDay = 31;
+    else if (month == 2 && isLeap) 
+        numberOfDay = 29;
+    else if (month == 2 && !isLeap)
+        numberOfDay = 28;
+    else
+        numberOfDay = 30;
+
+    if (day < 1 || day > numberOfDay)
+        return false;
+
+    return true;
 }
--- utils/Date.h
+++ utils/Date.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 using std::string;
-using std::to_string;
 
 struct Date {
     int day;
@@ -9,5 +8,6 @@ struct Date {
     int year;
     Date(){}
     Date(int day, int month, int year);
-    string toString();
+    string toString(const string delim="/");
+    bool valid();
 };
