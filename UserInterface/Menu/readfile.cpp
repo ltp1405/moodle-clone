bee0ebba1a1ee3706ea447546eb5791a38923ec8
nghiemkythu <nghiemdt2013@gmail.com>
2022-04-12 10:48:52 +0700

    fix readfile and savefile

--- UserInterface/Menu/readfile.cpp
+++ UserInterface/Menu/readfile.cpp
@@ -24,11 +24,37 @@ void App::readfile(){
             getline(fin, course_new->teacherName);
             fin >> course_new->credits;
             fin >> course_new->maxStudents;
+            int sizesession;
+            fin >> sizesession;
+            for(int k = 0; k < sizesession; k++){
+                Session *session_new = new Session;
+                int a, b;
+                fin >> a >> b;
+                session_new->day = (Day)a;
+                session_new->time = (Time)b;
+                course_new->sessions.addTail(session_new);
+            }
+            int sizestudent;
+            fin >> sizestudent;
+            for(int k = 0; k < sizestudent; k++){
+                Student *student_new = new Student;
+                string id;
+                fin.ignore();
+                getline(fin, id);
+                for(int i = 0; i < studentList.getSize(); i++){
+                    if(studentList[i]->data->id == id){
+                        student_new = studentList[i]->data;
+                        break;
+                    }
+                }
+                course_new->students.addTail(student_new);
+            }
             semester_new->courses.addTail(course_new);
         }
         S->semesters.addTail(semester_new);
     }
     schoolyears.addTail(S);
     currentSchoolyear = schoolyears.getTail()->data;
+    currentSemester = currentSchoolyear->semesters.getTail()->data;
     fin.close();
 }
