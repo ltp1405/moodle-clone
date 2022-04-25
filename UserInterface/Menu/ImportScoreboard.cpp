5eaee199af0b41aaf25ca94c99e0030114ffa979
ltp1405 <LamTungPro1405@gmail.com>
2022-04-25 19:24:28 +0700

    🔨 Handle multiple import case

--- UserInterface/Menu/ImportScoreboard.cpp
+++ UserInterface/Menu/ImportScoreboard.cpp
@@ -29,6 +29,21 @@ void App::promptImportCourseScoreboard() {
     fin.close();
     vvs file = readCSV(ans);
     for (int i = 1; i < file.size(); i++) {
+        bool skip = false;
+        for (int j = 0; j < file[i].size(); j++) {
+            if (file[0][j] == "student id") {
+                for (DNode<Score> *cur = scoreboard.getHead(); cur; cur = cur->next) {
+                    if (cur->data.id == file[i][j]) {
+                        cout << "Student " << cur->data.id << " already added." << endl;
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (skip)
+            continue;
         scoreboard.addTail(Score());
         for (int j = 0; j < file[i].size(); j++) {
             if (file[0][j] == "midterm mark")
